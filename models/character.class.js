7120bd5d115204b4bf9efb8835cb92fd0e7f2513
Jörg Eisenbraun <jorg_7@hotmail.com>
2026-02-17 16:25:31 +0100

    character.js edit

--- models/character.class.js
+++ models/character.class.js
@@ -137,12 +137,16 @@ class Character extends MovableObject {
     else if (this.isAboveGround()) this.playAnimation(this.IMAGES_JUMPING);
     else if (this.longIdle()) this.playAnimation(this.IMAGES_LONGIDLE);
     else if (this.idle()) this.playAnimation(this.IMAGES_IDLE);
-    else if (this.walkAnimations()) this.playAnimation(this.IMAGES_WALKING);
+    else {
+    if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+    this.playAnimation(this.IMAGES_WALKING);
+    }
+    } 
   }
 
-  walkAnimations() {
-    this.world.keyboard.RIGHT || this.world.keyboard.LEFT;
-  }
+//   walkAnimations() {
+//     this.world.keyboard.RIGHT || this.world.keyboard.LEFT;
+//   }
 
   updateIdleTime() {
     if (this.x === this.lastX) {
